3afb6887eb623ea464f11f1ada873331cd84a397
BrownNPC <deaddogfuneral@gmail.com>
2025-09-05 19:06:33 +0500

    completed setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,12 @@
 // Import required psp boilerplate
 #include "pspinit.h"
-
 #include "raylib.h"
-#include <print>
 int main(void) {
+  // 2.6x smaller than 1280x720
   const int screenWidth = 480;
   const int screenHeight = 270;
-  std::print("Hello, {}!\n", "world");
-  InitWindow(screenWidth, screenHeight, "Flappy Bird");
+
+  InitWindow(screenWidth, screenHeight, "Change this in main.cpp");
 
   while (!WindowShouldClose()) {
     BeginDrawing();
